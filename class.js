3786f2e03984bca2c128859f78dacf161b284427
Happiness-James <gesarehjames@gmail.com>
2022-07-15 11:50:42 +0300

    first

--- class.js
+++ class.js
@@ -12,6 +12,6 @@ class KioskCalc{
 }
 let kiosk = new KioskCalc({ "orange": 30, "mango": 15, "avocado": 40 })
 KioskCalc.prototype.getTotalCost = function(fruit,quantity){
-    return `${quantity} ${fruit} for ksh ${quantity*this.fruitsPriceList.orange}`
+    return `${quantity} ${fruit} for ksh ${quantity*this.fruitsPriceList[fruit]}`
 } 
-console.log(kiosk.getTotalCost("orange",6))
+console.log(kiosk.getTotalCost("mango",4))
--- objects.js
+++ objects.js
@@ -1,9 +1,7 @@
 let kiosk = {
-        Orange:30,
-        Mango:20,
-        Banana:10,
+        fruitList:{Orange:30,Mango:20,Banana:10},
         price:function(fruit,quantity){
-            return `${quantity} Orange for ksh. ${quantity* this.Orange}`
+            return `${quantity} ${fruit} for ksh. ${this.fruitList[fruit]*quantity}`
         }
 }
-console.log(kiosk.price("Orange",2))
+console.log(kiosk.price("Mango",2))
